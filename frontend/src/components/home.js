11b64f0f11433e4848310c41571f28769959be1e
wafaehamdaoui <hamdaouiwafae2000@gmail.com>
2023-01-15 22:20:16 +0330

    commit

---
+++ frontend/src/components/home.js
@@ -0,0 +1,58 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function Home() {
+  return (
+    <Carousel variant="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="ress2.jfif"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="gym.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="terr.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="salle.jpg"
+          alt="fourist slide"
+        />
+        <Carousel.Caption>
+          <h5>fourist slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Home;
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+ 
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+ 
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+ 
+// Here, we display our Navbar
+export default function Navbar() {
+ return (
+   <div>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+       <NavLink className="navbar-brand" to="/">
+       <img style={{"width" : 80 + '%'}} src="logo.png"></img>
+       </NavLink>
+       <button
+         className="navbar-toggler"
+         type="button"
+         data-toggle="collapse"
+         data-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span className="navbar-toggler-icon"></span>
+       </button>
+ 
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav mr-auto">
+           <li className="nav-item ">
+           <NavLink className="nav-link" to="/">
+               Home 
+             </NavLink>
+           </li><li className="nav-item ">
+             <NavLink className="nav-link" to="/create">
+               Create Account
+             </NavLink>
+           </li>
+           <li className="nav-item ">
+             <NavLink className="nav-link" to="/record">
+               User List
+             </NavLink>
+           </li>
+           <li className="nav-item ">
+             <NavLink className="nav-link" to="/record">
+               Request List
+             </NavLink>
+           </li>
+           <li className="nav-item ">
+             <NavLink className="nav-link" to="/">
+               Logout
+             </NavLink>
+           </li>
+         </ul>
+       </div>
+     </nav>
+   </div>
+ );
+}
