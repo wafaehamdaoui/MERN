46efe819acc3bff6eafd556ccbff7da7ba1ae442
wafaehamdaoui <75392302+wafaehamdaoui@users.noreply.github.com>
2023-01-24 13:24:50 +0330

    Update register.js

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -24,7 +24,7 @@ export default function Register() {
 	  // When a post request is sent to the create url, we'll add a new record to the database.
 	  const newUser = { ...form };
 	
-	  await fetch("http://localhost:5000/register", {
+	  await fetch("https://uemf-ressources-api-4150.onrender.com/register", {
 		method: "POST",
 		headers: {
 		  "Content-Type": "application/json",
