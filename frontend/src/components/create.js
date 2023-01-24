159b86e184f12db565072b929f5bc6fdcd8bacdd
wafaehamdaoui <75392302+wafaehamdaoui@users.noreply.github.com>
2023-01-24 13:14:03 +0330

    Update create.js

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -29,7 +29,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newDemande = { ...form };
  
-   await fetch("http://localhost:5000/record/add", {
+   await fetch("https://uemf-ressources-api-4150.onrender.com/record/add", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
