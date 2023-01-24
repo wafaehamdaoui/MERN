d674c45ad2d1e9480084a576d9c411f88eba02ab
wafaehamdaoui <75392302+wafaehamdaoui@users.noreply.github.com>
2023-01-24 14:02:57 +0330

    Update create.js

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -28,18 +28,25 @@ export default function Create() {
  
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newDemande = { ...form };
- 
-   await fetch("https://uemf-ressources-api-4150.onrender.com/record/add", {
+   try{
+   const res = await fetch("https://uemf-ressources-api-4150.onrender.com/record/add", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
      },
      body: JSON.stringify(newDemande),
    })
-   .catch(error => {
+   const data = await res.json();
+   if(data.error){
+    alert(data.error)
+   }else{
+    navigate("/student/record");
+   }
+  }catch(error){
      window.alert(error);
+     navigate("/student");
      return;
-   });
+   };
  
    setForm({ matricul: "",
    nom: "",
@@ -49,15 +56,14 @@ export default function Create() {
    duree: "",
    date: "",
    status: "En Attente", });
-   navigate("/student/record");
  }
  
  // This following section will display the form that takes the input from the user.
  return (
    <div>
-     <h3 style={{marginLeft:"20%"}}>  Demander une Ressource</h3>
+     <h3 style={{marginLeft:"40%",marginTop:"5%"}}>  Demander une Ressource</h3>
      <form onSubmit={onSubmit}>
-       <div className="col-md-6" style={{marginLeft:"20%"}}>
+       <div className="col-md-6" style={{marginLeft:"26%"}}>
          <label htmlFor="matricul" >Matricul</label>
          <input
            type="text"
@@ -67,7 +73,7 @@ export default function Create() {
            onChange={(e) => updateForm({ matricul: e.target.value })}
          />
        </div>
-       <div className="col-md-6" style={{marginLeft:"20%"}}>
+       <div className="col-md-6" style={{marginLeft:"26%"}}>
          <label htmlFor="nom" >Nom</label>
          <input
            type="text"
@@ -77,7 +83,7 @@ export default function Create() {
            onChange={(e) => updateForm({ nom: e.target.value })}
          />
        </div>
-       <div className="col-md-6" style={{marginLeft:"20%"}}>
+       <div className="col-md-6" style={{marginLeft:"26%"}}>
          <label htmlFor="prenom">Prenom</label>
          <input
            type="text"
@@ -87,8 +93,8 @@ export default function Create() {
            onChange={(e) => updateForm({ prenom: e.target.value })}
          />
        </div>
-       <label htmlFor="ecole" style={{marginLeft:"20%"}}>Ecole</label>
-       <div className="col-md-6" style={{marginLeft:"20%"}}>
+       <label htmlFor="ecole" style={{marginLeft:"26%"}}>Ecole</label>
+       <div className="col-md-6" style={{marginLeft:"26%"}}>
          <div className="form-check form-check-inline">
            <input
              className="form-check-input"
@@ -150,8 +156,8 @@ export default function Create() {
            <label htmlFor="ecoleEBS" className="form-check-label">EBS</label>
          </div>
        </div>
-       <label htmlFor="resource" style={{marginLeft:"20%"}}>Ressource</label>
-       <div className="col-md-6" style={{marginLeft:"20%"}}>
+       <label htmlFor="resource" style={{marginLeft:"26%"}}>Ressource</label>
+       <div className="col-md-6" style={{marginLeft:"26%"}}>
          <Form.Select aria-label="Default select example" onSelect={(e) => updateForm({ ressource: e.target.value })} onChange={(e) => updateForm({ ressource: e.target.value })}>
             <option>Selectionner une Ressource </option>
          <option value="Foot-ball" selected={form.ressource === "Foot-ball"}>Terrain Foot-ball</option>
@@ -168,19 +174,32 @@ export default function Create() {
             
           </Form.Select>
        </div>
-       <label htmlFor="resource" style={{marginLeft:"20%"}}>Durée</label>
-       <div className="col-md-6" style={{marginLeft:"20%"}}>
-                        <Form.Group controlId="duedate" >
-                            <Form.Control type="time" name="time" placeholder="Due time" onSelect={(e) => updateForm({ duree: e.target.value })} />
-                        </Form.Group>
-        </div>
-       <label htmlFor="resource" style={{marginLeft:"20%"}}>Date</label>
-       <div className="col-md-6" style={{marginLeft:"20%"}}>
+       <label htmlFor="resource" style={{marginLeft:"26%"}}>Durée</label>
+       <div className="col-md-6" style={{marginLeft:"26%"}}>
+         <Form.Select aria-label="Default select example" onSelect={(e) => updateForm({ duree: e.target.value })} onChange={(e) => updateForm({ duree: e.target.value })}>
+            <option>Selectionner une durée </option>
+         <option value="09:00 -- 10:00" selected={form.duree === "09:00 -- 10:00"}>09:00 -- 10:00</option>
+         <option value="10:00 -- 11:00" selected={form.duree === "10:00 -- 11:00"}>10:00 -- 11:00</option>
+         <option value="11:00 -- 12:00" selected={form.duree === "11:00 -- 12:00"}>11:00 -- 12:00</option>
+         <option value="12:00 -- 13:00" selected={form.duree === "12:00 -- 13:00"}>12:00 -- 13:00</option>
+         <option value="13:00 -- 14:00" selected={form.duree === "13:00 -- 14:00"}>13:00 -- 14:00</option>
+         <option value="14:00 -- 15:00" selected={form.duree === "14:00 -- 15:00"}>14:00 -- 15:00</option>
+         <option value="15:00 -- 16:00" selected={form.duree === "15:00 -- 16:00"}>15:00 -- 16:00</option>
+         <option value="16:00 -- 17:00" selected={form.duree === "16:00 -- 17:00"}>16:00 -- 17:00</option>
+         <option value="17:00 -- 18:00" selected={form.duree === "17:00 -- 18:00"}> 17:00 -- 18:00</option>
+         <option value="18:00 -- 19:00" selected={form.duree === "18:00 -- 19:00"}> 18:00 -- 19:00</option>
+         <option value="19:00 -- 20:00" selected={form.duree === "19:00 -- 20:00"}>19:00 -- 20:00</option>
+         <option value="20:00 -- 21:00" selected={form.duree === "20:00 -- 21:00"}> 20:00 -- 21:00</option>
+         <option value="21:00 -- 22:00" selected={form.duree === "21:00 -- 22:00"}>21:00 -- 22:00</option> 
+          </Form.Select>
+       </div>
+       <label htmlFor="resource" style={{marginLeft:"26%"}}>Date</label>
+       <div className="col-md-6" style={{marginLeft:"26%"}}>
                         <Form.Group controlId="duedate" >
                             <Form.Control type="date" name="date" placeholder="Due date" onSelect={(e) => updateForm({ date: e.target.value })}/>
                         </Form.Group>
                     </div>
-       <div className="col-md-4" style={{marginLeft:"40%",marginTop:"2%"}}>
+       <div className="col-md-4" style={{marginLeft:"45%",marginTop:"2%"}}>
          <input
            type="submit"
            value="Create Demande"
