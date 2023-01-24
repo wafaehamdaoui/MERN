0779fe3f109219204f8dc6dc688c00d28ca7fb10
wafaehamdaoui <75392302+wafaehamdaoui@users.noreply.github.com>
2023-01-24 13:22:36 +0330

    Update navbar2.js

--- frontend/src/components/navbar2.js
+++ frontend/src/components/navbar2.js
@@ -9,7 +9,7 @@ import { NavLink } from "react-router-dom";
 // Here, we display our Navbar
 export default function Navbar2() {
   async function handleLogout() {
-    await fetch(`http://localhost:5000/logout`, {
+    await fetch(`https://uemf-ressources-api-4150.onrender.com/logout`, {
       method: "post"
     });
   }
